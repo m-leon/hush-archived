f36eee84c8ba9926d3fcd524eef43db8225857d7
Max <max_leon@gmx.com>
2018-08-21 20:12:12 -0700

    - Created db_store folder to store persistent data for MariaDB - Created initial model and migrations for encrypted_posts

---
+++ src/app/EncryptedPost.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EncryptedPost extends Model
+{
+    protected $table = 'encrypted_posts';
+    protected $fillable = array('key', 'ciphertext');
+}
