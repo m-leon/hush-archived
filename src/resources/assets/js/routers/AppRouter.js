f0695bfd6af35816327d8a32184fdaf5303b95f1
Max <max_leon@gmx.com>
2018-09-03 09:53:23 -0700

    - Added default PHP ini files for different environments

--- src/resources/assets/js/routers/AppRouter.js
+++ src/resources/assets/js/routers/AppRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Example    from '../components/Example';
+import Example from '../components/Example';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
