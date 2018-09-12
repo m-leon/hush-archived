324c166b9dd6fb6a0f346efd61702067e51409d8
Max <max_leon@gmx.com>
2018-09-11 17:34:58 -0700

    - Removed Example component - Set NewPost as home

--- src/resources/assets/js/routers/AppRouter.js
+++ src/resources/assets/js/routers/AppRouter.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Example  from '../components/Example';
 import NotFound from '../components/NotFound';
 import NewPost  from '../components/NewPost';
 import ViewPost from '../components/ViewPost';
@@ -10,7 +9,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 const AppRouter = () => (
   <BrowserRouter>
     <Switch>
-      <Route path='/' component={Example} exact={true} />
+      <Route path='/' component={NewPost} exact={true} />
       <Route path='/new' component={NewPost} />
       <Route path='/view/:id' component={ViewPost} />
       <Route component={NotFound} />
