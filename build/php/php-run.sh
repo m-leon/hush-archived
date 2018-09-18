c0393fba100a26eb0e20002c6fc637d37a74a3d9
Max <max@maxleon.net>
2018-09-18 14:04:43 -0700

    - Added message to show when PHP service is delayed started

--- build/php/php-run.sh
+++ build/php/php-run.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 cd /app
 
+# Wait till DB is started
+until php artisan check:db; do
+  echo "Waiting for database connection to start"
+  sleep 5
+done
+
 php artisan migrate
 
 php-fpm
