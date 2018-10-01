1b46e5c7f3fc03d7bd88427d440a356638936629
Max <max@maxleon.net>
2018-10-01 13:34:24 -0700

    - Split NewPost into multiple smaller files

--- src/resources/assets/js/components/NewPost.js
+++ src/resources/assets/js/components/NewPost.js
@@ -1,8 +1,7 @@
-import AES    from 'crypto-js/aes';
-import axios  from 'axios';
-import moment from 'moment';
 import React  from 'react';
-import uuid   from 'uuid/v4';
+
+import NewPostURL from './NewPostURL';
+import { submit }   from '../utils/SendForm';
 
 export default class NewPost extends React.Component {
   constructor(props) {
@@ -12,84 +11,8 @@ export default class NewPost extends React.Component {
       error: '',            // Outputs errors to user
       formDisabled: false,  // Prevents user from double posting, disables the submit button once you initiate the process
       id: '',               // Holds the returned from the server once the ciphertext is submitted
-      key: '',              // Holds the key from the form
-      postURL: ''           // Holds the URL of the post INCLUDING the key generated from generatePostURL
+      key: ''               // Holds the key from the form
     };
-
-    this.submitForm = this.submitForm.bind(this);
-    this.sendCipher = this.sendCipher.bind(this);
-    this.generatePostURL = this.generatePostURL.bind(this);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.id !== this.state.id) {
-      this.generatePostURL();
-    }
-  }
-
-  submitForm(e) {
-    e.preventDefault();
-    // Use key if provided by user, if no key provided generate with UUIDV4
-    const key = (e.target.elements.key.value) ? e.target.elements.key.value : uuid();
-    const clear = e.target.elements.clear.value;
-
-    try {
-      const cipher = AES.encrypt(clear, key).toString();
-
-      // Update state values to be submitted
-      this.setState({
-        formDisabled: true,
-        key
-      });
-
-      // If we generated a key, update the form so the user can see it
-      e.target.elements.key.value = key;
-
-      // Calculate expiration date
-      const formExpiration = e.target.elements.expiration.value;
-       // Default to expire after 1 view
-      let expiration = 0;
-      // Calculate the requested expiration time
-      if (formExpiration !== '0') {
-        expiration = moment().add(1, formExpiration);
-        expiration = expiration.unix();
-      }
-
-      // Send cipher to server, no other data needs to be sent
-      this.sendCipher({ cipher, expiration });
-    } catch (e) {
-      // Failed to encrypt text. Maybe bad key
-      this.setState({ 'error': 'Failed to encrypt.' });
-    }
-  }
-
-  sendCipher(data) {
-    axios.post(`/api/post/`, data).then(
-      (res) => {
-        // Successfully posted
-        if (typeof(res.data.status) !== 'undefined' && res.data.status === '0') {
-          this.setState({ id: res.data.id, error: '' });
-        // Server didn't return with a status == 0, unknown error
-        } else {
-          this.setState({ error: 'Failed to post. Try again later.', formDisabled: false });
-        }
-      }
-    ).catch(
-      // Catch network errors
-      (e) => {
-        this.setState({ error: 'Failed to post. Try again later.', formDisabled: false })
-      }
-    );
-  }
-
-  generatePostURL(id) {
-    // For now use error to display post's URL
-    let hostname = window.location.hostname;
-    if (window.location.port && (window.location.port !== '80' || window.location.port != '443')) {
-      hostname += ':' + window.location.port;
-    }
-    const postURL = `${hostname}/view/${this.state.id}/#${this.state.key}`;
-    this.setState({ postURL, error: '' })
   }
 
   render() {
@@ -97,13 +20,14 @@ export default class NewPost extends React.Component {
       <div className="newPost">
         <h3 className="newPost__title">Post Your Message</h3>
         { this.state.error && <p>{this.state.error}</p> }
-        { this.state.postURL &&
-          <div>
-            <p className="newPost__url">This post is available at:</p>
-            <p className="newPost__url"><a href={`//${this.state.postURL}`}>{this.state.postURL}</a></p>
-          </div>
-        }
-        <form onSubmit={this.submitForm}>
+        <NewPostURL id={this.state.id} encKey={this.state.key} />
+        <form onSubmit={
+          async (e) => {
+            this.setState({ formDisabled: true });
+            const newState = await submit(e);
+            this.setState({ ...newState });
+          }
+        }>
           <textarea
             className="newPost__message"
             disabled={this.state.formDisabled}
---
+++ src/resources/assets/js/components/NewPostURL.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const NewPostURL = (props) => {
+  let hostname = window.location.hostname;
+  if (window.location.port && (window.location.port !== '80' || window.location.port != '443')) {
+    hostname += ':' + window.location.port;
+  }
+  const postURL = `${hostname}/view/${props.id}/#${props.encKey}`;
+
+  return (
+    <div>
+      { (props.id && props.encKey) &&
+        <div>
+          <p className="newPost__url">This post is available at:</p>
+          <p className="newPost__url"><a href={`//${postURL}`}>{postURL}</a></p>
+        </div>
+      }
+    </div>
+  );
+}
+
+export default NewPostURL;
