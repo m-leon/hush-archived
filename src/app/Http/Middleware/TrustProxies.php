039875af72caf62641720fdead7eba044b9b9e93
Max <max@maxleon.net>
2018-10-01 11:47:13 -0700

    - Hardcoded "192.168.X.X" into the trusted proxies so the docker instance can be proxied to behind HTTPS

--- src/app/Http/Middleware/TrustProxies.php
+++ src/app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies;
+    protected $proxies = ['192.168.0.0/16'];
 
     /**
      * The headers that should be used to detect proxies.
