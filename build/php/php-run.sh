de5e3b27684191075e18cc9db7326a35ac714bc5
Max <max@maxleon.net>
2019-01-27 14:52:14 -0800

    - Updated docker machines to new version - Fixed permission issues on 2nd and 3rd runs in php-run

--- build/php/php-run.sh
+++ build/php/php-run.sh
@@ -4,7 +4,7 @@ cd /app
 # Correct permissions on Laravel's cache directories and database
 chown -R www-data:www-data /app/storage
 chown -R www-data:www-data /app/bootstrap/cache
-chown www-data:www-data ${DB_DATABASE}
+chown www-data:www-data /tmp/hush.db
 
 DOCKER_HOST=$(/sbin/ip route|awk '/default/ { print $3 }')
 cat >/usr/local/etc/php-fpm.d/env.conf <<EOL
